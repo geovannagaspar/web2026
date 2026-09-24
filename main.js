c0a7a2bee424d029d65abf54bae1f20985cecf6a
geovannagaspar <geovannagasparino3@gmail.com>
2026-09-24 09:12:02 -0300

    Implement form submission and sum calculation
    Added event listener to handle form submission and calculate the sum of two numbers.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-const formulario = document.getElementById("calcForm")
+const formulario = document.getElementById("calcForm")/*pegando o formulário*/
 
 formulario.addEventListener("submit", function(e){
-    
-}
+    e.preventDefault();/*impede que a página seja recarregada*/ 
+
+    const a = Number(document.getElementById("num1").value)
+    const b = Number(document.getElementById("num2").value)
+
+    const soma = a+b
+
+    document.getElementById("resultado").textContent = soma
+})
